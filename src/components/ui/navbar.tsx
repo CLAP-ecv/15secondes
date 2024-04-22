8f4d8de583ee4232f1d7d4228928de1698d18e83
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2024-04-22 09:33:00 +0200

    :sparkles: Add button component and update

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -7,9 +7,9 @@ export type NavbarProps = {
 
 export const Navbar = (props: NavbarProps) => {
   return (
-    <div className="flex items-center justify-center bg-white w-full h-[var(--header-height)] rounded-b-lg shadow-lg sticky top-0">
+    <nav className="flex items-center justify-center bg-white w-full h-[var(--header-height)] rounded-b-lg shadow-lg sticky top-0">
         <Image src={"/15s.png"} alt="15Secondes" width={50} height={50}  />
         <Image src={SearchIcon} alt="Icone de recherche" width={25} height={25} className="absolute top-5 right-5"/>
-    </div>
+    </nav>
   )
 }
