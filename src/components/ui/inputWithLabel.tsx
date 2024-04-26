8b33c2a51924ae246754f8f82f9b013757abd853
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-04-26 16:42:26 +0200

    :triangular_flag_on_post: Add description props.

--- src/components/ui/inputWithLabel.tsx
+++ src/components/ui/inputWithLabel.tsx
@@ -4,13 +4,19 @@ import { Label } from "@/src/components/ui/label";
 
 export interface InputWithLabelProps extends InputProps{
     label: string
+    description?: string
 }
 
 export const InputWithLabel = (props: InputWithLabelProps) => {
     return (
         <div className={props.className}>
             <Label htmlFor={props.id}>{props.label}</Label>
-            <Input type={props.type} id={props.id} name={props.name} placeholder={props.placeholder} />
+            <Input type={props.type} id={props.id} name={props.name} placeholder={props.placeholder} accept={props.accept} />
+            {
+                props.description && 
+                <small>{props.description}</small>
+            }
+            
         </div>
     );
 }
