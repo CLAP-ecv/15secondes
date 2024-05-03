12ca17894e89d796e8e90cd73f0ed6e12c5e2fe1
Aubrian <aubrian.duhayon@mail-ecv.fr>
2024-05-03 13:45:33 +0200

    :sparkles: Add first version of autocomplete input

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,40 +1,45 @@
-import { createSearchParamsCache, parseAsString } from "nuqs/server";
-import { prisma } from "../layout";
 import { ArticleVideoCard } from "@/src/components/ArticleVideoCard/ArticleVideoCard";
+import { prisma } from "../layout";
+import { Suspense } from "react";
+import { createSearchParamsCache, parseAsString } from "nuqs/server";
 
 const searchParamsCache = createSearchParamsCache({
     q: parseAsString.withDefault('').withOptions({
-        clearOnDefault: true
+        clearOnDefault: true,
     })
 })
 
 export type SearchPageProps = {
 }
 
-export default async function SearchPage({ searchParams }: { searchParams: Record<string, string | string[] | undefined> }) {
+export default async function SearchPage({ searchParams }: { searchParams: { q: string } }) {
+
 
     const { q } = searchParamsCache.parse(searchParams)
 
     const filteredArticles = await prisma.article.findMany({
         where: {
             title: {
-                contains: Buffer.from(q, "ascii").toString()
+                contains: q
             }
         }
     })
 
     return (
         <main className="p-5">
-            <h1 className="font-bold text-lg">Résultat(s) de recherche pour : <br /><q>{Buffer.from(q, "ascii").toString()}</q></h1>
+            <h1 className="font-bold text-lg">Résultat(s) de recherche pour : <br /><q>{searchParams.q}</q></h1>
             <ul className="space-y-5">
-                {filteredArticles.map((article) => (
-                    <ArticleVideoCard
-                        key={article.id}
-                        article={article}
-                        variant="no-date"
-                    />
-                ))}
+                <Suspense fallback={"Chargement ..."}>
+                    {filteredArticles.map((article) => (
+                        <ArticleVideoCard
+                            key={article.id}
+                            article={article}
+                            variant="no-date"
+                        />
+                    ))}
+                </Suspense>
             </ul>
+
         </main>
     )
 }
