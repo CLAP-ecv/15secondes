5785a9042b7ab6a610547904ff3a60cf9f2286eb
Aubrian <aubrian.duhayon@mail-ecv.fr>
2024-05-17 12:22:03 +0200

    :sparkles: Add onesignal

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren, useEffect } from "react"
 import OneSignal from 'react-onesignal';
 
 async function runOneSignal() {
-    await OneSignal.init({ appId: '9567164b-cb9c-42a5-b14a-a0cf341af31b', allowLocalhostAsSecureOrigin: true });
+    await OneSignal.init({ appId: '9567164b-cb9c-42a5-b14a-a0cf341af31b' });
     OneSignal.Slidedown.promptPush();
 }
 
