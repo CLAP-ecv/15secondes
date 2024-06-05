9b7c497bed3093be17e9c1b6d20ee136e641fc7e
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2024-06-05 22:23:06 +0200

    :sparkles: Add OneSignal working config

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,21 +1,33 @@
 "use client"
 
-import { PropsWithChildren, useEffect } from "react"
-import OneSignal from 'react-onesignal';
-
-async function runOneSignal() {
-    await OneSignal.init({ appId: '9567164b-cb9c-42a5-b14a-a0cf341af31b' });
-    OneSignal.Slidedown.promptPush();
-}
+import Script from "next/script";
+import { PropsWithChildren } from "react";
 
 export const Providers = (props: PropsWithChildren) => {
-    useEffect(() => {
-        runOneSignal();
-    }, []);
-
     return (
         <>
+            <Script
+                id="onesignal-sdk"
+                src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+                onLoad={initOneSignal}
+            />
             {props.children}
         </>
     )
+}
+
+const initOneSignal = () => {
+    // @ts-ignore
+    const onesignal = window.OneSignal
+    if (!onesignal) {
+        console.warn('OneSignal SDK not loaded.')
+        return
+    }
+    onesignal.init({
+        appId: "9567164b-cb9c-42a5-b14a-a0cf341af31b",
+        notifyButton: {
+            enable: true,
+        },
+        allowLocalhostAsSecureOrigin: true
+    });
 }
