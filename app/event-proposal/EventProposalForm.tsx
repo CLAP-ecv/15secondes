e0b29a412b92855bb462efc498cf842dd75b3fd6
Aubrian <aubrian.duhayon@mail-ecv.fr>
2024-06-07 10:34:13 +0200

    :sparkles: Create event form

---
+++ app/event-proposal/EventProposalForm.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { Button } from "@/src/components/ui/button"
+import { InputWithLabel } from "@/src/components/ui/inputWithLabel"
+import { Label } from "@/src/components/ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/src/components/ui/select"
+import { TextareaWithLabel } from "@/src/components/ui/textareaWithLabel"
+import { eventPropsalAction } from "./event-proposal.action"
+
+export default function EventProposalForm() {
+    return (
+        <form className="mt-12 mb-10 flex gap-4 flex-col" action={async (formData: FormData) => { eventPropsalAction(formData) }}>
+            <h2 className="text-xl font-bold">Vous</h2>
+            <InputWithLabel label="Prénom *" name="firstName" id="firstName" type="text" placeholder="Alex" required />
+            <InputWithLabel label="Nom *" name="lastName" id="lastName" type="text" placeholder="Dupont" required />
+            <InputWithLabel label="Email *" name="email" id="email" type="email" placeholder="alex.dupont@exemple.com" required />
+            <h2 className="text-xl font-bold">Votre évènement</h2>
+            <div>
+                <Label htmlFor="theme">Thématique *</Label>
+                <Select name="theme" required>
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Sélectionner une thématique" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {/* @ts-ignore */}
+                        <SelectItem value={null}>Toutes les thématiques</SelectItem>
+                        <SelectItem key={"Concert"} value={"Concert"}>{"Concert"}</SelectItem>
+                        <SelectItem key={"Activité"} value={"Activité"}>{"Activité"}</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            <InputWithLabel label="Nom de l’événement *" name="eventName" id="eventName" type="text" placeholder="Concert de Jazz" required />
+            <div>
+                <Label htmlFor="eventCity">Ville de l'évènement</Label>
+                <Select name="eventCity">
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Sélectionner une ville" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {/* @ts-ignore */}
+                        <SelectItem value={null}>Toutes les villes</SelectItem>
+                        <SelectItem key={"Lille"} value={"Lille"}>{"Lille (59)"}</SelectItem>
+                        <SelectItem key={"Paris"} value={"Paris"}>{"Paris (75)"}</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            <InputWithLabel label="Date de début *" name="startDate" id="startDate" type="datetime-local" required />
+            <InputWithLabel label="Date de fin" name="endDate" id="endDate" type="datetime-local" />
+            <TextareaWithLabel label="Description de l'évènement *" name="eventDescription" id="eventDescription" placeholder="Dites-nous en plus" rows={5} required />
+            <InputWithLabel label="Image d'illustration" name="image" id="image" type="file" accept="image/png, image/jpeg" description="Formats acceptés : JPG ou PNG ; taille optimale : 1600x900px" />
+
+            <InputWithLabel label="Lien vers votre site ou réseau" name="linkToWebsiteOrMedia[]" id="linkToWebsiteOrMedia" type="text" placeholder="https://www.votresite.com" />
+            <InputWithLabel label="Titre du lien à afficher" name="titleToWebsiteOrMedia[]" id="titleToWebsiteOrMedia" type="text" placeholder="Mon programmme" />
+
+            <Button className="mx-auto mt-8" size="lg" type="submit">Envoyer votre évènement</Button>
+        </form>
+    )
+}
--- app/event-proposal/page.tsx
+++ app/event-proposal/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -6,16 +5,11 @@ import {
     BreadcrumbList,
     BreadcrumbPage,
     BreadcrumbSeparator,
-} from "@/src/components/ui/breadcrumb"
-import { Button } from "@/src/components/ui/button";
-import { InputWithLabel } from "@/src/components/ui/inputWithLabel";
-import { TextareaWithLabel } from "@/src/components/ui/textareaWithLabel";
-import Image from "next/image";
-import { eventPropsalAction } from "./event-proposal.action";
-import Link from "next/link";
+} from "@/src/components/ui/breadcrumb";
 import dayjs from "dayjs";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/src/components/ui/select";
-import { Label } from "@/src/components/ui/label";
+import Image from "next/image";
+import Link from "next/link";
+import EventProposalForm from "./EventProposalForm";
 
 const thisMonth = dayjs().format("YYYY-MM");
 
@@ -67,51 +61,7 @@ export default function EventProposalPage({ searchParams }: { searchParams: { st
                         Les champs marqués d'une étoile (*) sont requis pour soumettre votre événement.
                     </i>
 
-                    <form className="mt-12 mb-10 flex gap-4 flex-col" action={async (formData: FormData) => { eventPropsalAction(formData) }}>
-                        <h2 className="text-xl font-bold">Vous</h2>
-                        <InputWithLabel label="Prénom *" name="firstName" id="firstName" type="text" placeholder="Alex" required />
-                        <InputWithLabel label="Nom *" name="lastName" id="lastName" type="text" placeholder="Dupont" required />
-                        <InputWithLabel label="Email *" name="email" id="email" type="email" placeholder="alex.dupont@exemple.com" required />
-                        <h2 className="text-xl font-bold">Votre évènement</h2>
-                        <div>
-                            <Label htmlFor="theme">Thématique *</Label>
-                            <Select name="theme" required>
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Sélectionner une thématique" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {/* @ts-ignore */}
-                                <SelectItem value={null}>Toutes les thématiques</SelectItem>
-                                <SelectItem key={"Concert"} value={"Concert"}>{"Concert"}</SelectItem>
-                                <SelectItem key={"Activité"} value={"Activité"}>{"Activité"}</SelectItem>
-                            </SelectContent>
-                            </Select>
-                        </div>
-                        <InputWithLabel label="Nom de l’événement *" name="eventName" id="eventName" type="text" placeholder="Concert de Jazz" required />
-                        <div>
-                            <Label htmlFor="eventCity">Ville de l'évènement</Label>
-                            <Select name="eventCity">
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Sélectionner une ville" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {/* @ts-ignore */}
-                                <SelectItem value={null}>Toutes les villes</SelectItem>
-                                <SelectItem key={"Lille"} value={"Lille"}>{"Lille (59)"}</SelectItem>
-                                <SelectItem key={"Paris"} value={"Paris"}>{"Paris (75)"}</SelectItem>
-                            </SelectContent>
-                            </Select>
-                        </div>
-                        <InputWithLabel label="Date de début *" name="startDate" id="startDate" type="date" required />
-                        <InputWithLabel label="Date de fin" name="endDate" id="endDate" type="date" />
-                        <TextareaWithLabel label="Description de l'évènement *" name="eventDescription" id="eventDescription" placeholder="Dites-nous en plus" rows={5} required/>
-                        <InputWithLabel label="Image d'illustration" name="image" id="image" type="file" accept="image/png, image/jpeg" description="Formats acceptés : JPG ou PNG ; taille optimale : 1600x900px" />
-                        
-                        <InputWithLabel label="Lien vers votre site ou réseau" name="linkToWebsiteOrMedia[]" id="linkToWebsiteOrMedia" type="text" placeholder="https://www.votresite.com" />
-                        <InputWithLabel label="Titre du lien à afficher" name="titleToWebsiteOrMedia[]" id="titleToWebsiteOrMedia" type="text" placeholder="Mon programmme" />
-
-                        <Button className="mx-auto mt-8" size="lg" type="submit">Envoyer votre évènement</Button>
-                    </form>
+                    <EventProposalForm />
                 </div>
             </>
         )
